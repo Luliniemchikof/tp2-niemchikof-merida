f16eaa75f4aaf1511e02550c9ceeb80cf19b5fae
Thomas <88115844+Thomerida@users.noreply.github.com>
2025-06-29 14:43:52 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,17 @@
 
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'nombre_de_tu_base';
+
+$conn = new mysqli($host, $user, $password, $database);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+// Opcional: establecer codificación
+$conn->set_charset("utf8");
+?>
